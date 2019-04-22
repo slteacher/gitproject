709e779d4225f85632680600dd050d17389d88bb
slteacher <sl@itcast.cn>
2019-04-22 14:27:20 +0800

    添加一个User文件

--- testgit/src/main/java/com/itheima/pojo/User.java
+++ testgit/src/main/java/com/itheima/pojo/User.java
@@ -2,8 +2,8 @@ package com.itheima.pojo;
 
 /**
  * @author 黑马程序员
- * @Company http://www.ithiema.com
- * @Version 1.0
- */
+        * @Company http://www.ithiema.com
+        * @Version 1.0
+        */
 public class User {
 }
